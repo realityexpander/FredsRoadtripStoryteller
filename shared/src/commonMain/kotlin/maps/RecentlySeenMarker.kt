77a3d42882ad97aab6bdee85917399c154008cc2
Chris Athanas <realityexpander@gmail.com>
2023-11-03 00:13:54 -0500

    remove unused field

--- shared/src/commonMain/kotlin/maps/RecentlySeenMarker.kt
+++ shared/src/commonMain/kotlin/maps/RecentlySeenMarker.kt
@@ -7,11 +7,7 @@ data class RecentlySeenMarker(
     val id: MarkerIdStr,
     val title: String,
     val insertedAtEpochMilliseconds: Long = 0,
-
-    @Suppress("Unused") // will be used todo remove?
-    val seenOrder: Int = 0,
-) {
-}
+)
 
 @Serializable
 data class RecentlySeenMarkersList(
