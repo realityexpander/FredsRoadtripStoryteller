04c7cea4980f52fcb2c8b13d9b53ac9eefc2eb3a
Chris Athanas <realityexpander@gmail.com>
2023-11-14 01:23:01 -0600

    Add basic onboarding

--- shared/src/commonMain/kotlin/presentation/app/AppDrawerContent.kt
+++ shared/src/commonMain/kotlin/presentation/app/AppDrawerContent.kt
@@ -1,6 +1,7 @@
 package presentation.app
 
 import BottomSheetScreen
+import Onboarding
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomSheetScaffoldState
+import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -25,7 +27,11 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VolumeUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
@@ -44,6 +50,7 @@ fun AppDrawerContent(
     onSetBottomSheetActiveScreen: (BottomSheetScreen) -> Unit = {},
 ) {
     val coroutineScope = rememberCoroutineScope()
+    var isOnboardingDialogVisible by remember { mutableStateOf(false)}
 
     // Header
     Row(
@@ -76,6 +83,36 @@ fun AppDrawerContent(
 
     }
     Spacer(modifier = Modifier.height(16.dp))
+
+    Button(
+        onClick = {
+            coroutineScope.launch {
+                bottomSheetScaffoldState.drawerState.close()
+                isOnboardingDialogVisible = true
+            }
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+    ) {
+        Text(
+            "Show Onboarding",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp),
+            fontStyle = FontStyle.Normal,
+            fontSize = MaterialTheme.typography.body1.fontSize,
+            textAlign = TextAlign.Center,
+        )
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    if(isOnboardingDialogVisible) {
+        Onboarding(
+            onDismiss = {
+                isOnboardingDialogVisible = false
+            }
+        )
+    }
 
     // Show all loaded markers
     Row(
---
+++ shared/src/commonMain/kotlin/presentation/onboarding/onboarding.kt
@@ -0,0 +1,214 @@
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalResourceApi::class)
+@Composable
+fun Onboarding(
+    onDismiss: () -> Unit = {}
+) {
+    val pages = listOf(
+        "onboarding_page1.png",
+        "onboarding_page2.png",
+        "onboarding_page3.png",
+    )
+
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f
+    ) {
+        // provide pageCount
+        pages.size
+    }
+    val scope = rememberCoroutineScope()
+
+    Dialog(
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false,
+        ),
+        onDismissRequest = {
+            onDismiss()
+        },
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            HorizontalPager(
+                modifier = Modifier,
+                state = pagerState,
+                pageSpacing = 0.dp,
+                userScrollEnabled = true,
+                reverseLayout = false,
+                contentPadding = PaddingValues(0.dp),
+                beyondBoundsPageCount = 0,
+                pageSize = PageSize.Fill,
+                flingBehavior = PagerDefaults.flingBehavior(state = pagerState),
+                key = { pages[it] },
+                pageNestedScrollConnection = PagerDefaults.pageNestedScrollConnection(
+                    Orientation.Horizontal
+                ),
+                pageContent = { index ->
+                    Image(
+                        painter = painterResource(res = pages[index]),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            )
+            Box(
+                modifier = Modifier
+                    .offset(y = -(16).dp)
+                    .fillMaxWidth(0.5f)
+                    .clip(RoundedCornerShape(100))
+                    .background(MaterialTheme.colors.background)
+                    .padding(8.dp)
+                    .align(Alignment.BottomCenter)
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(
+                                pagerState.currentPage - 1
+                            )
+                        }
+                    },
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowLeft,
+                        contentDescription = "Go back",
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
+
+                DotsIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center),
+                    totalDots = pages.size,
+                    selectedIndex = pagerState.currentPage,
+                    selectedColor = MaterialTheme.colors.onBackground,
+                    unSelectedColor = MaterialTheme.colors.onBackground.copy(alpha = 0.25f)
+                )
+
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(
+                                pagerState.currentPage + 1
+                            )
+                        }
+                    },
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowRight,
+                        contentDescription = "Go forward",
+                        tint = MaterialTheme.colors.onBackground
+                    )
+                }
+            }
+
+            // Close Button
+            IconButton(
+                onClick = {
+                    onDismiss()
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopEnd)
+                    .background(
+                        MaterialTheme.colors.surface.copy(alpha = 0.5f),
+                        shape = MaterialTheme.shapes.medium
+                    )
+                    .clickable { onDismiss() }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Close",
+                    modifier = Modifier.alpha(0.8f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color,
+    unSelectedColor: Color,
+) {
+    LazyRow(
+        modifier = modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+
+    ) {
+
+        items(totalDots) { index ->
+            if (index == selectedIndex) {
+                Box(
+                    modifier = Modifier
+                        .size(8.dp)
+                        .clip(CircleShape)
+                        .background(selectedColor)
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .size(8.dp)
+                        .clip(CircleShape)
+                        .background(unSelectedColor)
+                )
+            }
+
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+            }
+        }
+    }
+}
