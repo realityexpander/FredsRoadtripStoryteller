ce04263327092a46b5b67f2abde9e55ce0cf7fbe
Chris Athanas <realityexpander@gmail.com>
2023-11-22 14:26:35 -0600

    Add search dialogin app drawer

--- shared/src/commonMain/kotlin/presentation/app/AppDrawerContent.kt
+++ shared/src/commonMain/kotlin/presentation/app/AppDrawerContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -20,7 +21,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.BottomSheetScaffoldState
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
@@ -49,6 +49,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusProperties
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontStyle
@@ -56,6 +58,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import kAppNameStr
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.yield
@@ -76,14 +80,14 @@ fun AppDrawerContent(
     val coroutineScope = rememberCoroutineScope()
 
     val entries: SnapshotStateList<Marker> = remember { mutableStateListOf() }
-    val searchEntries = remember { mutableStateListOf<Marker>() }
+    val searchMarkers = remember { mutableStateListOf<Marker>() }
     LaunchedEffect(finalMarkers) {
         // Log.d("📌📌📌AppDrawerContent: LaunchedEffect(finalMarkers) calculating entries...")
         entries.clear()
         entries.addAll(finalMarkers.reversed())
 
-        searchEntries.clear()
-        searchEntries.addAll(finalMarkers.reversed())
+        searchMarkers.clear()
+        searchMarkers.addAll(finalMarkers.reversed())
     }
 
     // App title & close button
@@ -171,36 +175,34 @@ fun AppDrawerContent(
 
     // Search box - rounded text field
     var searchQuery by remember { mutableStateOf("") }
-    val keyboardController = LocalSoftwareKeyboardController.current
+    var isSearchDialogVisible by remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
             .clip(CircleShape)
             .background(MaterialTheme.colors.onBackground.copy(alpha = 0.1f))
-
     ) {
         TextField(
             modifier = Modifier
-                .fillMaxWidth(),
-            value = searchQuery,
-            onValueChange = {
-                searchQuery = it
-
-                if(searchQuery.isEmpty()) {
-                    searchEntries.clear()
-                    searchEntries.addAll(finalMarkers.reversed())
-                } else {
-                    searchEntries.clear()
-                    searchEntries.addAll(
-                        finalMarkers.filter { marker ->
-                            marker.title.contains(searchQuery, ignoreCase = true)
-                        }.sortedBy { marker ->
-                            marker.title
-                        }
-                    )
+                .fillMaxWidth()
+                .onFocusChanged { focusState ->
+                    if (focusState.isFocused) {
+                        isSearchDialogVisible = true
+                    }
                 }
-            },
+                .focusProperties {
+                    if(isSearchDialogVisible) {
+                        canFocus = false
+                    }
+                }
+            ,
+            placeholder = { Text(text = "Search Marker Title...") },
+            value = searchQuery,
+            onValueChange = { },
+            readOnly = true,
+            singleLine = true,
+            maxLines = 1,
             colors = TextFieldDefaults.textFieldColors(
                 backgroundColor = MaterialTheme.colors.background,
                 placeholderColor = MaterialTheme.colors.onBackground.copy(alpha = 0.5f),
@@ -216,8 +218,8 @@ fun AppDrawerContent(
                     IconButton(
                         onClick = {
                             searchQuery = ""
-                            searchEntries.clear()
-                            searchEntries.addAll(finalMarkers.reversed())
+                            searchMarkers.clear()
+                            searchMarkers.addAll(finalMarkers.reversed())
                         }
                     ) {
                         Icon(
@@ -227,15 +229,6 @@ fun AppDrawerContent(
                     }
                 }
             },
-            placeholder = { Text(text = "Search Title...") },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = androidx.compose.ui.text.input.ImeAction.Search
-            ),
-            singleLine = true,
-            maxLines = 1,
-            keyboardActions = KeyboardActions {
-                keyboardController?.hide()
-            }
         )
     }
     Spacer(modifier = Modifier.height(8.dp))
@@ -292,6 +285,30 @@ fun AppDrawerContent(
         )
     }
 
+    if(isSearchDialogVisible) {
+        SearchMarkerDialog(
+            finalMarkers = finalMarkers,
+            onSearchResult = { query, searchMarkersResult ->
+                searchQuery = query
+                searchMarkers.clear()
+                searchMarkers.addAll(searchMarkersResult)
+                isSearchDialogVisible = false
+            },
+            onClickMarker = { marker ->
+                println("Clicked on marker: ${marker.id}")
+            },
+            onLocateMarker = { marker ->
+                println("Locate marker: ${marker.id}")
+            },
+            onNavToMarker = { marker ->
+                println("Nav to marker: ${marker.id}")
+            },
+            onDismiss = {
+                isSearchDialogVisible = false
+            }
+        )
+    }
+
     LazyColumn(
         modifier = Modifier
             .fillMaxHeight(),
@@ -301,9 +318,9 @@ fun AppDrawerContent(
         // Header
 
 //        items(entries.size) { markerIdx ->
-        items(searchEntries.size) { markerIdx ->
+        items(searchMarkers.size) { markerIdx ->
 //            val marker = entries[markerIdx]
-            val marker = searchEntries[markerIdx]
+            val marker = searchMarkers[markerIdx]
 
             Row(
                 modifier = Modifier
@@ -378,6 +395,238 @@ fun AppDrawerContent(
                     fontStyle = FontStyle.Normal,
                     fontSize = MaterialTheme.typography.body1.fontSize,
                 )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun SearchMarkerDialog(
+    finalMarkers: List<Marker>,
+    onSearchResult: (searchQuery: String, searchResult: List<Marker>) -> Unit =
+        { _, _ -> },
+    onClickMarker: (Marker) -> Unit = {},
+    onLocateMarker: (Marker) -> Unit = {},
+    onNavToMarker: (Marker) -> Unit = {},
+    onDismiss: () -> Unit = {},
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Dialog(
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false,
+        ),
+        onDismissRequest = {
+            onDismiss()
+        },
+    ) {
+        var searchQuery by remember { mutableStateOf("") }
+        val searchEntries = remember { mutableStateListOf<Marker>().also {
+                it.addAll(finalMarkers.reversed())
+            } }
+        val keyboardController = LocalSoftwareKeyboardController.current
+
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(MaterialTheme.colors.onBackground.copy(alpha = 0.1f))
+                .clickable {
+                    keyboardController?.show()
+                }
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                // Close button
+                IconButton(
+                    onClick = {
+                        onSearchResult(searchQuery, searchEntries)
+                        onDismiss()
+                    },
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .align(Alignment.End)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close",
+                        tint = MaterialTheme.colors.onBackground.copy(alpha = 0.5f)
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colors.onBackground.copy(alpha = 0.1f))
+                ) {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        placeholder = { Text(text = "Search Marker Title...") },
+                        value = searchQuery,
+                        onValueChange = {
+                            searchQuery = it
+
+                            if (searchQuery.isEmpty()) {
+                                searchEntries.clear()
+                                searchEntries.addAll(finalMarkers.reversed())
+                            } else {
+                                searchEntries.clear()
+                                searchEntries.addAll(
+                                    finalMarkers.filter { marker ->
+                                        marker.title.contains(searchQuery, ignoreCase = true)
+                                    }.sortedBy { marker ->
+                                        marker.title
+                                    }
+                                )
+                            }
+                        },
+                        singleLine = true,
+                        maxLines = 1,
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = MaterialTheme.colors.background,
+                            placeholderColor = MaterialTheme.colors.onBackground.copy(alpha = 0.5f),
+                            leadingIconColor = MaterialTheme.colors.onBackground,
+                            textColor = MaterialTheme.colors.onBackground,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            cursorColor = MaterialTheme.colors.onBackground,
+                        ),
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = ""
+                            )
+                        },
+                        trailingIcon = {
+                            if (searchQuery.isNotEmpty()) {
+                                IconButton(
+                                    onClick = {
+                                        searchQuery = ""
+                                        searchEntries.clear()
+                                        searchEntries.addAll(finalMarkers.reversed())
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Close,
+                                        contentDescription = "Clear search"
+                                    )
+                                }
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = androidx.compose.ui.text.input.ImeAction.Search
+                        ),
+                        keyboardActions = KeyboardActions {
+                            keyboardController?.hide()
+                        }
+                    )
+                }
+            }
+
+            // List of markers
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(top = 112.dp),
+                state = rememberLazyListState(),
+                userScrollEnabled = true,
+            ) {
+
+                items(searchEntries.size) { markerIdx ->
+                    val marker = searchEntries[markerIdx]
+
+                    if(searchEntries.size == 0) {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            "No markers found",
+                            modifier = Modifier.padding(start = 8.dp),
+                            fontSize = MaterialTheme.typography.h6.fontSize,
+                            fontWeight = FontWeight.Normal,
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .animateItemPlacement(animationSpec = tween(250))
+                            .clickable {
+                                coroutineScope.launch {
+                                    println("Clicked on marker: ${marker.id}")
+                                }
+                            },
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text(
+                            text = marker.title,
+                            modifier = Modifier.weight(2.5f),
+                            softWrap = false,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            fontStyle = FontStyle.Normal,
+                            fontSize = MaterialTheme.typography.body1.fontSize,
+                        )
+
+                        // isSeen
+                        if (marker.isSeen) {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Seen",
+                                modifier = Modifier
+                                    .weight(.3f)
+                                    .height(16.dp)
+                            )
+                        } else {
+                            // "leave blank"
+                            Spacer(
+                                modifier = Modifier
+                                    .padding(end = 2.dp)
+                                    .weight(.3f)
+                                    .height(16.dp)
+                            )
+                        }
+
+                        // isSpoken
+                        if (marker.isSpoken) {
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Spoken",
+                                modifier = Modifier
+                                    .weight(.3f)
+                                    .height(16.dp)
+                            )
+                        } else {
+                            // "leave blank"
+                            Spacer(
+                                modifier = Modifier
+                                    .padding(end = 2.dp)
+                                    .weight(.3f)
+                                    .height(16.dp)
+                            )
+                        }
+
+                        Text(
+                            text = marker.id,
+                            modifier = Modifier
+                                .padding(end = 8.dp)
+                                .weight(1.2f),
+                            fontStyle = FontStyle.Normal,
+                            fontSize = MaterialTheme.typography.body1.fontSize,
+                        )
+                    }
+                }
             }
         }
     }
